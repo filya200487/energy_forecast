a9003a64c7398d06eab0e623eac79807797ff966
filya200487 <135419701+filya200487@users.noreply.github.com>
2023-09-05 00:19:11 +0300

    Update main.py
    corrections

--- main.py
+++ main.py
@@ -5,27 +5,28 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from main_class import EnergyForecast
 
+
 class EnergyForecastApp:
     def __init__(self, root):
         self.root = root
         self.root.title('Прогноз потребления энергии')
         
-        self.file_label = tk.Label(root, text = "Загрузите файл с данными:")
+        self.file_label = tk.Label(root, text="Загрузите файл с данными:")
         self.file_label.pack()
         
-        self.load_button = tk.Button(root, text = "Загрузить файл", command = self.load_data)
+        self.load_button = tk.Button(root, text="Загрузить файл", command = self.load_data)
         self.load_button.pack()
         
-        self.years_label = tk.Label(root, text = "Введите количество лет для прогноза:")
+        self.years_label = tk.Label(root, text="Введите количество лет для прогноза:")
         self.years_label.pack()
         
         self.years_entry = tk.Entry(root)
         self.years_entry.pack()
         
-        self.forecast_button = tk.Button(root, text = "Сделать прогноз", command = self.make_forecast)
+        self.forecast_button = tk.Button(root, text="Сделать прогноз", command=self.make_forecast)
         self.forecast_button.pack()
         
-        self.canvas = FigureCanvasTkAgg(Figure(), master = root)
+        self.canvas = FigureCanvasTkAgg(Figure(), master=root)
         self.canvas.get_tk_widget().pack()
         self.energy_forecast = EnergyForecast(self.canvas)
         
@@ -46,4 +47,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = EnergyForecastApp(root)
     app.run()
-
