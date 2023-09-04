fd1d27b0abbbabbe9f3b848b59ac94aee8261e3c
filya200487 <135419701+filya200487@users.noreply.github.com>
2023-09-04 23:59:46 +0300

    Update main.py
    Исправление ошибок построения графика

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from main_class import EnergyForecast
